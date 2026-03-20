84b241eed683797cc4484910adc4f4dff781ce56
Rakieft <kieftraphter.joly1@gmail.com>
2026-03-20 11:39:35 -0400

    fix server structure

--- school-api/server.js
+++ school-api/server.js
@@ -7,19 +7,25 @@ const { connectDB } = require("./database/connection");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger/swagger.json");
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Connect DB
 connectDB();
 
+// Routes
 app.use("/students", require("./routes/studentsRoutes"));
 app.use("/courses", require("./routes/coursesRoutes"));
 
-app.listen(process.env.PORT, () => {
-  console.log("🚀 Server running");
-});
-
+// Swagger 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Port
+const PORT = process.env.PORT || 8080;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
